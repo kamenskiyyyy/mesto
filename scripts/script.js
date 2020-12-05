788ed42183daa9853a4fb0a38d31cf8bff82bbbd
Kamenev Nikolay <72261703+kamenskiyyyy@users.noreply.github.com>
2020-12-05 21:55:16 +0300

    feat: add popup new element

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 let profileEditButtonNode = document.querySelector('.profile__edit-button');
-let popupNode = document.querySelector('.popup');
+let elementAddButtonNode = document.querySelector('.profile__add-button');
+let popupEditNode = document.querySelector('.profile-edit');
+let popupNewElementNode = document.querySelector('.new-element');
 let popupCloseButtonNode = document.querySelector('.popup__close-button');
+let popupNewElementCloseButtonNode = document.querySelector('.popup__close-button-elem');
 let popupSaveButtonNode = document.querySelector('.form__button');
 
 let profileNameNode = document.querySelector('.profile__name');
@@ -9,23 +12,69 @@ let formNode = document.querySelector('.form');
 let nameInput = document.querySelector('.form__name');
 let jobInput = document.querySelector('.form__job');
 
-function openPopupVisibility() {
-    popupNode.classList.add('popup__view');
+const elementsConteiner = document.querySelector('.elements');
+const titleInput = document.querySelector('.form__title');
+const urlInput = document.querySelector('.form__url');
+const templateElement = document.querySelector('.template')
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+
+
+function openPopupEditProfileVisibility() {
+    popupEditNode.classList.add('popup__view');
     nameInput.value = profileNameNode.textContent;
     jobInput.value = profileJobNode.textContent;
-}
+};
 
 function closePopupVisibility() {
-    popupNode.classList.remove('popup__view');
-}
+    popupEditNode.classList.remove('popup__view');
+};
+
+function openPopupAddElementVisibility() {
+    popupNewElementNode.classList.add('popup__view');
+    // добавить imput
+};
+
+function closePopupAddElementVisibility() {
+    popupNewElementNode.classList.remove('popup__view');
+};
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileNameNode.textContent = nameInput.value;
     profileJobNode.textContent = jobInput.value;
     closePopupVisibility();
-}
+};
 
-profileEditButtonNode.addEventListener('click', openPopupVisibility);
+profileEditButtonNode.addEventListener('click', openPopupEditProfileVisibility);
 popupCloseButtonNode.addEventListener('click', closePopupVisibility);
 formNode.addEventListener('submit', formSubmitHandler);
+elementAddButtonNode.addEventListener('click', openPopupAddElementVisibility);
+popupNewElementCloseButtonNode.addEventListener('click', closePopupAddElementVisibility);
