39f6922d693b552286d9e8eceb7e50a2bb6541ad
Kamenev Nikolay <79969997909@mail.ru>
2020-12-09 18:55:23 +0300

    feat: bag fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
 const profileEditButtonNode = document.querySelector('.profile__edit-button');
 const elementAddButtonNode = document.querySelector('.profile__add-button');
-const popupEditNode = document.querySelector('.profile-edit');
-const popupNewElementNode = document.querySelector('.new-element');
-const popupImageNode = document.querySelector('.img-full');
-const popupSaveButtonNode = document.querySelector('.form__button');
+const popupEditNode = document.querySelector('#profile-edit');
+const popupNewElementNode = document.querySelector('#new-element');
+const popupImageNode = document.querySelector('#img-full');
 
 const profileNameNode = document.querySelector('.profile__name');
 const profileJobNode = document.querySelector('.profile__job');
@@ -12,17 +11,13 @@ const nameInput = document.querySelector('.form__name');
 const jobInput = document.querySelector('.form__job');
 
 const elementsConteiner = document.querySelector('.elements');
-const elementNameNode = document.querySelector('.element__name');
-const elementUrlNode = document.querySelector('.element__img');
 const titleInput = document.querySelector('.form__title');
 const urlInput = document.querySelector('.form__url');
 const templateElement = document.querySelector('.template');
 
-
 const popupCloseButtonNode = document.querySelector('#close_profile_edit');
 const popupNewElementCloseButtonNode = document.querySelector('#close_new_element');
 const popupImageBodeCloseButton = document.querySelector('#close_img_full');
-const popupObertka = document.querySelector('.popup__close-button');
 
 const initialCards = [
     {
@@ -85,29 +80,25 @@ function renderElements() {
     elementsConteiner.append(...elementCard);
 };
 
-function openPopupEditProfileVisibility() {
-    popupEditNode.classList.add('popup__view');
+function openPopup(popup) {
+    popup.classList.add('popup__view');
+}
+
+function closePopup(popup) {
+    popup.classList.remove('popup__view');
+}
+
+function openPopupEditProfileVisibility(popup) {
+    popup.classList.add('popup__view');
     nameInput.value = profileNameNode.textContent;
     jobInput.value = profileJobNode.textContent;
-};
-
-function closePopupVisibility() {
-    popupEditNode.classList.remove('popup__view');
-};
-
-function openPopupAddElementVisibility() {
-    popupNewElementNode.classList.add('popup__view');
-};
-
-function closePopupAddElementVisibility() {
-    popupNewElementNode.classList.remove('popup__view');
 };
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileNameNode.textContent = nameInput.value;
     profileJobNode.textContent = jobInput.value;
-    closePopupVisibility();
+    closePopup(popupEditNode);
 };
 
 function bindAddItemElement() {
@@ -123,7 +114,7 @@ function addNewItem(evt) {
     elementsConteiner.prepend(newItem);
     titleInput.value = '';
     urlInput.value = '';
-    closePopupAddElementVisibility();
+    closePopup(popupNewElementNode);
 };
 
 function removeItem(evt) {
@@ -145,24 +136,12 @@ function openImagePopup(evt) {
     popupTitle.textContent = evt.target.data;
 };
 
-function closeImagePopup() {
-    popupImageNode.classList.remove('popup__view');
-};
-
-// function closePopapNewVersion(event) {
-//     if (event.target === classList('popup__close-button')) {
-//         event.closest.classList.remove('popup__view');
-//     }
-// }
-
-profileEditButtonNode.addEventListener('click', openPopupEditProfileVisibility);
-popupCloseButtonNode.addEventListener('click', closePopupVisibility);
 formNode.addEventListener('submit', formSubmitHandler);
-elementAddButtonNode.addEventListener('click', openPopupAddElementVisibility);
-popupNewElementCloseButtonNode.addEventListener('click', closePopupAddElementVisibility);
-popupImageBodeCloseButton.addEventListener('click', closeImagePopup);
-// popupObertka.addEventListener('click', closePopapNewVersion);
+profileEditButtonNode.addEventListener('click', () => openPopupEditProfileVisibility(popupEditNode));
+popupCloseButtonNode.addEventListener('click', () => closePopup(popupEditNode));
+elementAddButtonNode.addEventListener('click', () => openPopup(popupNewElementNode));
+popupNewElementCloseButtonNode.addEventListener('click', () => closePopup(popupNewElementNode));
+popupImageBodeCloseButton.addEventListener('click', () => closePopup(popupImageNode));
 
 renderElements();
 bindAddItemElement();
-
