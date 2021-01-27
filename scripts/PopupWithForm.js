4f686d4f8a763e6cd3deacee794e7c8915b4933d
Kamenev Nikolay <79969997909@mail.ru>
2021-01-27 13:26:01 +0300

    Update PopupWithForm.js

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -1,5 +1,32 @@
-export default class PopupWithForm {
-    constructor(params) {
-        
-    }
+import Popup from "./Popup.js";
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, { handleFormSubmit }) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector('.popup__form');
+    this._setEventListeners();
+  }
+
+  _getInputValues() {
+    this._inputList = this._form.querySelectorAll('.popup__input');
+    this._formValues = {};
+    this._inputList.forEach(input => {
+      this._formValues[input.name] = input.value;
+    });
+    return this._formValues;
+  }
+
+  _setEventListeners() {
+    this._form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+      this.close();
+    });
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
 }
